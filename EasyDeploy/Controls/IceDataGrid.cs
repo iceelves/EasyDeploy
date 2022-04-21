e10491f6926caf81e34c49d0fcc051e01d0f0d96
Sadness96 <qsbs623@163.com>
2022-04-21 21:26:28 +0800

    新增：表格样式

---
+++ EasyDeploy/Controls/IceDataGrid.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace EasyDeploy.Controls
+{
+    [TemplatePart(Name = "Part_Path", Type = typeof(FrameworkElement))]
+    public class IceDataGrid : DataGrid
+    {
+        static IceDataGrid()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(IceDataGrid), new FrameworkPropertyMetadata(typeof(IceDataGrid)));
+        }
+    }
+}
--- EasyDeploy/MainWindow.xaml.cs
+++ EasyDeploy/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using EasyDeploy.Helpers;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
