726bb5bef1e849722d1ed6e4e66ec74b82e6612f
Sadness96 <qsbs623@163.com>
2022-04-25 11:42:32 +0800

    新增：绝对路径相对路径转换

---
+++ src/EasyDeploy/Helpers/PathHelper.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace EasyDeploy.Helpers
+{
+    /// <summary>
+    /// 文件路径相关帮助类
+    /// </summary>
+    public static class PathHelper
+    {
+        /// <summary>
+        /// 是否为绝对路径
+        /// </summary>
+        /// <param name="Path">路径</param>
+        /// <returns>绝对路径返回True,相对路径返回False</returns>
+        public static bool IsAbsolutePath(string Path)
+        {
+            return Path.Contains(":\\") || Path.Contains(":/");
+        }
+
+        /// <summary>
+        /// 绝对路径转相对路径
+        /// </summary>
+        /// <param name="AbsolutePath">绝对路径</param>
+        /// <param name="CurrentPath">当前路径</param>
+        /// <returns>相对路径</returns>
+        public static string AbsoluteToRelative(string AbsolutePath, string CurrentPath = null)
+        {
+            if (string.IsNullOrEmpty(CurrentPath))
+            {
+                CurrentPath = Environment.CurrentDirectory;
+            }
+            string[] absoluteDirectories = CurrentPath.Split('\\');
+            string[] relativeDirectories = AbsolutePath.Split('\\');
+
+            //Get the shortest of the two paths
+            int length = absoluteDirectories.Length < relativeDirectories.Length ? absoluteDirectories.Length : relativeDirectories.Length;
+
+            //Use to determine where in the loop we exited
+            int lastCommonRoot = -1;
+            int index;
+
+            //Find common root
+            for (index = 0; index < length; index++)
+                if (absoluteDirectories[index] == relativeDirectories[index])
+                    lastCommonRoot = index;
+                else
+                    break;
+
+            //If we didn't find a common prefix then throw
+            if (lastCommonRoot == -1)
+                throw new ArgumentException("Paths do not have a common base");
+
+            //Build up the relative path
+            StringBuilder relativePath = new StringBuilder();
+
+            //Add on the ..
+            for (index = lastCommonRoot + 1; index < absoluteDirectories.Length; index++)
+                if (absoluteDirectories[index].Length > 0)
+                    relativePath.Append("..\\");
+
+            //Add on the folders
+            for (index = lastCommonRoot + 1; index < relativeDirectories.Length - 1; index++)
+                relativePath.Append(relativeDirectories[index] + "\\");
+            relativePath.Append(relativeDirectories[relativeDirectories.Length - 1]);
+
+            return relativePath.ToString();
+        }
+
+        /// <summary>
+        /// 相对路径转绝对路径
+        /// </summary>
+        /// <param name="RelativePath">相对路径</param>
+        /// <param name="CurrentPath">当前路径</param>
+        /// <returns>绝对路径</returns>
+        public static string RelativeToAbsolute(string RelativePath, string CurrentPath = null)
+        {
+            if (string.IsNullOrEmpty(CurrentPath))
+                return Path.GetFullPath(RelativePath);
+            else
+                return Path.GetFullPath(RelativePath, CurrentPath);
+        }
+    }
+}
