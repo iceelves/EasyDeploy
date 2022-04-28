5faf99529e9d8da7020940e3e003b5f3f8cfd192
Sadness96 <qsbs623@163.com>
2022-04-29 00:38:04 +0800

    新增：自定义控制台控件

---
+++ src/EasyDeploy/Controls/IceRichTextBox.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace EasyDeploy.Controls
+{
+    [TemplatePart(Name = "Part_Path", Type = typeof(FrameworkElement))]
+    public class IceRichTextBox : RichTextBox
+    {
+        static IceRichTextBox() => DefaultStyleKeyProperty.OverrideMetadata(typeof(IceRichTextBox), new FrameworkPropertyMetadata(typeof(IceRichTextBox)));
+
+        /// <summary>
+        /// 添加文本
+        /// </summary>
+        /// <param name="Text"></param>
+        public void SetText(string Text)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Paragraph paragraph = new Paragraph();
+                paragraph.Inlines.Add(new Run() { Foreground = Brushes.Red, Text = "R" });
+                paragraph.Inlines.Add(new Run() { Foreground = Brushes.Green, Text = "G" });
+                paragraph.Inlines.Add(new Run() { Foreground = Brushes.Blue, Text = "B" });
+                this.Document.Blocks.Add(paragraph);
+            }
+        }
+    }
+}
