39cceeed4d106a32e2600f8e6a06d68576d8ba01
Sadness96 <qsbs623@163.com>
2022-05-03 13:57:11 +0800

    新增：CheckBox样式

---
+++ src/EasyDeploy/Controls/IceCheckBox.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace EasyDeploy.Controls
+{
+    [TemplatePart(Name = "Part_Path", Type = typeof(FrameworkElement))]
+    public class IceCheckBox : CheckBox
+    {
+        static IceCheckBox() => DefaultStyleKeyProperty.OverrideMetadata(typeof(IceCheckBox), new FrameworkPropertyMetadata(typeof(IceCheckBox)));
+    }
+}
